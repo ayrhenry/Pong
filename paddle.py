caeccfbc7e1a089624edbebad064a0696d69d1c0
Ayran <a.hen3000@gmail.com>
2024-07-14 17:47:26 -0500

    Finished game

---
+++ paddle.py
@@ -0,0 +1,22 @@
+from turtle import * 
+
+
+class Paddle(Turtle):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(x_pos, y_pos)
+
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
